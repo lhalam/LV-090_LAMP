f6af4ad7db1178c759e00b3a8326beb5f698335b
akohutc <kogut.andriy@gmail.com>
2013-06-25 19:11:18 +0300

    add config parser tests

---
+++ dbapi/config_parser_tests.py
@@ -0,0 +1,25 @@
+import unittest
+from config_parser import get_section_settings
+
+
+class TestConfigParsing(unittest.TestCase):
+
+    def setUp(self):
+        self.config_file = 'test_config.cfg'
+        self.section = "MySQL settings"
+
+    def test_section_parameters(self):
+        parameters = ('host', 'user', 'password', 'database_name', 'port')
+        for parameter in get_section_settings(self.config_file, self.section):
+            self.assertIn(parameter, parameters)
+
+    def test_section_parameter_values(self):
+        mysql_section_data = get_section_settings(self.config_file, self.section)
+        self.assertEquals(mysql_section_data['host'], 'localhost')
+        self.assertEquals(mysql_section_data['user'], 'root')
+        self.assertEquals(mysql_section_data['password'], 'root')
+        self.assertEquals(mysql_section_data['port'], '3306')
+
+
+if __name__ == '__main__':
+    unittest.main()
