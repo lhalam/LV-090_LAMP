5b7b55dd3d190f9cc86e175c20eeb5d6359e5877
akohutc <kogut.andriy@gmail.com>
2013-06-25 21:09:16 +0300

    implement loading logger settings from config file

--- dbapi/logger.py
+++ dbapi/logger.py
@@ -1,18 +1,30 @@
 import logging
 
+from config_parser import get_section_settings
 
-def create_logger(name, logfile):
+LOGGING_SECTION_NAME = 'Logging'
+
+
+def create_logger(name, config):
     """Create logger object"""
+    # get dictationary of config options for logger module
+    config_data = get_section_settings(config, LOGGING_SECTION_NAME)
+    level_name = config_data['level'].upper()
+    # get logging level and set it to corresponding attribute
+    level = logging.__getattribute__(level_name)
+    logfile = config_data['logfile']
+
+    # make nice formatting string
     log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(level)
 
     file_handler = logging.FileHandler(logfile)
-    file_handler.setLevel(logging.DEBUG)
+    file_handler.setLevel(level)
 
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.ERROR)
+    console_handler.setLevel(level)
 
     formatter = logging.Formatter(log_format)
     file_handler.setFormatter(formatter)
