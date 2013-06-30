2abad52ea18f117dbe4e59310630e04c981bd62b
andriykohut <kogut.andriy@gmail.com>
2013-06-30 11:55:06 +0300

    change pooling module to use config file

--- dbapi/pooling.py
+++ dbapi/pooling.py
@@ -1,23 +1,22 @@
 import sqlalchemy.pool as pool
 
 from mysql_connector import get_database_connection
+from config_parser import get_section_settings
 
 
-def create_pool(config, section, overflow, size):
+def create_pool(config):
     """Create a pool of database connections
 
     :param config: String with name of configuration file which contains
     connection settings.
-    :param section: Config section with mysql settings.
-    :param overflow: Maximum overflow size of the pool.
-    :param size: Largest number of connections that will be kept persistently
-    in the pool.
 
     """
-    get_connection = lambda: get_database_connection(config, section)
+    pool_settings = get_section_settings(config, 'Pooling')
     mysql_pool = pool.QueuePool(
-        get_connection,
-        max_overflow=overflow,
-        pool_size=size
+        lambda: get_database_connection(config, 'MySQL settings'),
+        pool_size=int(pool_settings['pool_size']),
+        max_overflow=int(pool_settings['max_overflow']),
+        timeout=int(pool_settings['timeout']),
+        recycle=int(pool_settings['recycle'])
     )
     return mysql_pool
